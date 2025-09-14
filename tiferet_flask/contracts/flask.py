d05ce3c904df7a7bea58f2751891462ba14437ea
Ashatz <andrew@greatstrength.me>
2025-09-13 17:54:21 -0700

    Contracts - Flask - Update Flask Contracts

--- tiferet_flask/contracts/flask.py
+++ tiferet_flask/contracts/flask.py
@@ -6,7 +6,8 @@ from typing import List
 # ** infra
 from tiferet.contracts import (
     ModelContract,
-    Repository
+    Repository,
+    abstractmethod
 )
 
 # *** contracts
@@ -16,14 +17,24 @@ class FlaskRouteContract(ModelContract):
     '''
     A contract for Flask route models.
     '''
-    pass
+    
+    # * attribute: id
+    id: str
+
+    # * attribute: status_code
+    status_code: int
 
 # ** contract: flask_blueprint_contract
 class FlaskBlueprintContract(ModelContract):
     '''
     A contract for Flask blueprint models.
     '''
-    pass
+    
+    # * attribute: id
+    id: str
+
+    # * attribute: routes
+    routes: List[FlaskRouteContract]
 
 # ** contract: flask_api_repository
 class FlaskApiRepository(Repository):
@@ -32,6 +43,7 @@ class FlaskApiRepository(Repository):
     '''
 
     # * method: get_blueprints
+    @abstractmethod
     def get_blueprints(self) -> List[FlaskBlueprintContract]:
         '''
         Retrieve all Flask blueprints.
@@ -39,4 +51,19 @@ class FlaskApiRepository(Repository):
         :return: A list of FlaskBlueprintContract instances.
         :rtype: List[FlaskBlueprintContract]
         '''
-        raise NotImplementedError
+        raise NotImplementedError('get_blueprints method not implemented.')
+    
+    # * method: get_route
+    @abstractmethod
+    def get_route(self, route_id: str, blueprint_id: str = None) -> FlaskRouteContract:
+        '''
+        Retrieve a specific Flask route by its blueprint and route IDs.
+
+        :param route_id: The ID of the route within the blueprint.
+        :type route_id: str
+        :param blueprint_id: The ID of the blueprint (optional).
+        :type blueprint_id: str
+        :return: The corresponding FlaskRouteContract instance.
+        :rtype: FlaskRouteContract
+        '''
+        raise NotImplementedError('get_route method not implemented.')
