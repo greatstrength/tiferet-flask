e6d7e138d529d4d9ee6d10fb88eb227f244e86bd
Andrew <andrew@greatstrength.me>
2025-09-24 13:10:46 -0700

    Tiferet Flask - Add Flask Models and Contracts - 10035493644 (#1)
    * Models - Flask - Add Flask Models
    
    * Models - Add Test Package
    
    * Models - Tests (Flask) - Add Tests for Flask Models
    
    * Contracts - Flask - Add Flask Contracts

---
+++ tiferet_flask/models/tests/test_flask.py
@@ -0,0 +1,76 @@
+# *** imports
+
+# ** infra
+import pytest
+
+# ** app
+from ..flask import *
+
+# *** fixtures
+
+# ** fixture: flask_route
+@pytest.fixture
+def flask_route() -> FlaskRoute:
+    '''
+    A fixture that provides a sample FlaskRoute instance for testing.
+
+    :return: A sample FlaskRoute instance.
+    :rtype: FlaskRoute
+    '''
+
+    return ModelObject.new(
+        FlaskRoute,
+        id='sample_route',
+        rule='/sample',
+        methods=['GET', 'POST'],
+        status_code=200
+    )
+
+# ** fixture: flask_blueprint
+@pytest.fixture
+def flask_blueprint(flask_route: FlaskRoute) -> FlaskBlueprint: 
+    '''
+    A fixture that provides a sample FlaskBlueprint instance for testing.
+
+    :param flask_route: A sample FlaskRoute instance.
+    :type flask_route: FlaskRoute
+    :return: A sample FlaskBlueprint instance.
+    :rtype: FlaskBlueprint
+    '''
+
+    return ModelObject.new(
+        FlaskBlueprint,
+        name='sample_blueprint',
+        routes=[flask_route]
+    )
+
+# *** tests
+
+# ** test: flask_route_creation
+def test_flask_route_creation(flask_route: FlaskRoute):
+    '''
+    Test the creation of a FlaskRoute instance.
+
+    :param flask_route: A sample FlaskRoute instance.
+    :type flask_route: FlaskRoute
+    '''
+
+    assert flask_route.id == 'sample_route'
+    assert flask_route.rule == '/sample'
+    assert flask_route.methods == ['GET', 'POST']
+    assert flask_route.status_code == 200
+
+# ** test: flask_blueprint_creation
+def test_flask_blueprint_creation(flask_blueprint: FlaskBlueprint, flask_route: FlaskRoute):
+    '''
+    Test the creation of a FlaskBlueprint instance.
+
+    :param flask_blueprint: A sample FlaskBlueprint instance.
+    :type flask_blueprint: FlaskBlueprint
+    :param flask_route: A sample FlaskRoute instance.
+    :type flask_route: FlaskRoute
+    '''
+
+    assert flask_blueprint.name == 'sample_blueprint'
+    assert len(flask_blueprint.routes) == 1
+    assert flask_blueprint.routes[0] == flask_route
